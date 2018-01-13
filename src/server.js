bf326aef64250cc6993ea23820dafdad138755f1
Hendra Dwi Saputra <babeguru@gmail.com>
2018-01-13 22:04:41 +0700

    Contoh Payload
    body yang biasa di express disebut dengan payload di hapijs

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
-import Hapi from 'hapi';
+import Hapi from "hapi";
 
 const server = new Hapi.Server({
-  host: 'localhost',
+  host: "localhost",
   port: 3000
 });
 
 server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: (request, reply) => {
-    return 'Hello World!';
+  method: "POST",
+  path: "/payload",
+  handler: (request, h) => {
+    return { body: request.payload.coba };
   }
 });
 
 server
   .start()
   .then(() => {
-    console.log(`Server started at ${server.info.uri}`)
+    console.log(`Server started at ${server.info.uri}`);
   })
-  .catch(err =>{
-    console.error('Error was handled!');
+  .catch(err => {
+    console.error("Error was handled!");
     console.error(err);
   });
